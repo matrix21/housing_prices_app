86feb4d5231d592ce24e9a9532b0e929f4ac2fce
n0g00zs <Nayan.Gupta@walmartlabs.com>
2020-10-08 19:24:10 +0530

    setup cookiecutter structure for the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Basic Hobby project to learn,build and deploy ML models as an end to end application',
+    author='nayan gupta',
+    license='MIT',
+)
